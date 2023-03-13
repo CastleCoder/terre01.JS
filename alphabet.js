ca2ca8ef24f49c754a01a1f0c1b74d06e8a88469
Cyrille <cyrillechateau@icloud.com>
2023-03-13 22:22:42 +0100

    update

--- alphabet.js
+++ alphabet.js
@@ -1,18 +1,23 @@
-const alphabetArray = ["abcdefghijklmnopqrstuvwxyz\n"];
-for(let i=0; i<alphabetArray.length;i++){
+function alphabet(){
 
-  console.log(alphabetArray[i]);
+  const alphabetArray = ["abcdefghijklmnopqrstuvwxyz\n"];
+  for(let i=0; i<alphabetArray.length;i++){
+
+    console.log(alphabetArray[i]);
+  }
+
+
+
+
+  const a = 97;
+  const z = 122;
+  let Array = [];
+
+  for (let i=a; i<=z; i++){
+    let result = String.fromCharCode(i);
+    let resultInArray = Array.push(result);
+  }
+  console.log(Array.join("")+"\n");
 }
 
-
-
-
-const a = 97;
-const z = 122;
-let Array = [];
-
-for (let i=a; i<=z; i++){
-  let result = String.fromCharCode(i);
-  let resultInArray = Array.push(result);
-}
-console.log(Array.join("")+"\n");
+alphabet();
